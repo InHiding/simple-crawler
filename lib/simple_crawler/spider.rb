c4d64409ed934fa2e867cf30f10f1e613ce6c54f
Giuliano Melo <giuliano.melo@geekie.com.br>
2017-04-16 23:22:48 -0300

    Handle errors

--- lib/simple_crawler/spider.rb
+++ lib/simple_crawler/spider.rb
@@ -1,12 +1,14 @@
 require "uri"
 require "oga"
 require "httparty"
+require "colorize"
 
 class Spider
     MAX_URLS = 1000
 
     def initialize(options = {})
         @root = nil
+        @visited_urls = Set.new
         @max_urls = options.fetch(:max_urls, MAX_URLS)
     end
 
@@ -14,14 +16,17 @@ class Spider
         return [] if start_url.nil? or start_url.empty?
 
         @root = URI.parse(start_url)
-        urls = [start_url]
+        urls = [@root]
 
         results = []
         while(urls.size > 0)
             url = urls.shift
+            next if @visited_urls.include? "#{url.host}#{url.path}"
+            puts "Crawling url: #{url}".colorize(:light_blue)
             page = get_page(url)
-            results << {url: url, assets: get_assets(page)}
+            results << {url: url.to_s, assets: get_assets(page)}
             urls.concat get_child_urls(page)
+            @visited_urls.add "#{url.host}#{url.path}"
         end
 
         results
@@ -33,11 +38,19 @@ class Spider
         uri = URI.parse(url)
         return uri if uri.absolute?
         URI.join(@root, url)
+    rescue URI::InvalidURIError => e
+        puts "#{url} is invalid. #{e.message}".colorize(:red)
+    rescue URI::InvalidComponentError => e
+        puts "#{url} isn't crawlable. #{e.message}".colorize(:red)
     end
 
     def get_page(url)
         response = HTTParty.get(url)
         Oga.parse_html(response.body)
+    rescue ArgumentError => e
+        puts "Invalid content for #{url}: #{e.message}".colorize(:red)
+    rescue StandardError => e
+        puts "Could not parse response for #{url}: #{e.message}".colorize(:red)
     end
 
     def get_assets(page)
@@ -47,19 +60,19 @@ class Spider
         page.css('link').each do |l|
             next unless l['href']
             url = normalize_url(l['href'])
-            assets << url.to_s if url.host == @root.host
+            assets << url.to_s if url and url.host == @root.host
         end
 
         page.css('script').each do |s|
             next unless s['src']
             url = normalize_url(s['src'])
-            assets << url.to_s if url.host == @root.host
+            assets << url.to_s if url and url.host == @root.host
         end
 
         page.css('img').each do |i|
             next unless i['src']
             url = normalize_url(i['src'])
-            assets << url.to_s if url.host == @root.host
+            assets << url.to_s if url and url.host == @root.host
         end
 
         assets
@@ -72,8 +85,10 @@ class Spider
         page.css('a').each do |l|
             break if urls.size == @max_urls
             next unless l['href']
+            next if l['href'].start_with? "#" #avoid anchors to same page
             url = normalize_url(l['href'])
-            urls << url.to_s if url.host == @root.host
+            # remove query params
+            urls << url if url and url.host == @root.host
         end
         urls
     end
